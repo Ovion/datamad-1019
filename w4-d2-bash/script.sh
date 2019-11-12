4a610a095248c1038a18243c5a0b03949f459271
Marc Pomar <marc@faable.com>
2019-11-12 15:43:18 +0100

    done

---
+++ w4-d2-bash/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# REMEMBER ADD EXECUTION PERMISSION TO THIS SCRIPT chmod +x script.sh
+NOMBRE="Pepe"
+SALUDO="Hola"
+
+COMPLETO=$(echo "$SALUDO me llamo $NOMBRE")
+echo $COMPLETO $COMPLETO
+
+## EJ1
+# ls -als
+
+## EJ2
+# cat playlist_kuboloco.txt | xargs youtube-dl
+
