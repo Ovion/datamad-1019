c58421c62d99f84a430ae90c76f9c95715ef3c4a
Marc Pomar <marc@faable.com>
2019-11-19 12:46:09 +0100

    api

---
+++ w5-d2-simple-api/app.py
@@ -0,0 +1,29 @@
+from bottle import route, run, get
+import random
+
+
+@get("/")
+def index():
+    return {
+        "nombre": random.choice(["Pepe", "Juan", "Fran", "Luis"])
+    }
+
+
+@get("/chiste/<tipo>")
+def demo2(tipo):
+    print(f"un chiste de {tipo}")
+    if tipo == "chiquito":
+        return {
+            "chiste": "Van dos soldados en una moto y no se cae ninguno porque van soldados"
+        }
+    elif tipo == "eugenio":
+        return {
+            "chiste": "Saben aquell que diu...."
+        }
+    else:
+        return {
+            "chiste": "No puedorrr!!"
+        }
+
+
+run(host='localhost', port=8080)
